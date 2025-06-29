baf393d9765afa397568bfb67334a58f553def2f
ashutosh <kurvheashutosh@gmail.com>
2025-06-29 21:50:10 +0530

    Making some changes

--- Personal Productivity OS/Server/controllers/note.controller.js
+++ Personal Productivity OS/Server/controllers/note.controller.js
@@ -1,6 +1,33 @@
 const Note = require("../models/Note");
 
-exports.addNote = async (req, res) => {};
+
+//Add note
+exports.addNote = async (req, res) => {
+    const userId = req.body.id;
+
+    try {
+        const { title, content, tags, projectId, summary } = req.body;
+        
+        //Validate require fields
+        if (!title || !content) {
+            return res.status(400).json({ message: 'All fields are required' });
+        }
+
+        //Create new note
+        const newNote = new Note({
+          title,
+          content,
+          tags,
+          projectId,
+          summary,
+        });
+
+        await newNote.save();
+        res.status(200).json(newNote);
+    } catch (error) {
+        console.status(500).json({ message: 'Sever Error' });
+    }
+};
 exports.getAllNotes = async (req, res) => {};
 exports.deleteNote = async (req, res) => {};
 exports.deleteAllNotes = async (req, res) => {};
--- Personal Productivity OS/Server/controllers/task.controller.js
+++ Personal Productivity OS/Server/controllers/task.controller.js
@@ -1,8 +1,60 @@
 const Task = require('../models/Task');
 
+//Add Task 
+exports.addTask = async (req, res) => {
+    const userId = req.user.id;
 
-exports.addTask = async (req , res) =>{}
-exports.getAllTasks = async (req , res) =>{}
-exports.deleteTask = async (req , res) =>{}
-exports.deleteAllTasks = async (req , res) =>{}
+    try {
+        const { title, description, projectId, status, priority, dueDate, orderIndex } = req.body;
+        
+        //Validate requirement fields
+        if (!title || !description) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
+
+        //Create new task
+        const newTask = new Task({
+            userId,
+            title,
+            description,
+            projectId,
+            status,
+            priority,
+            date: new Date(dueDate),
+            orderIndex,
+        });
+
+        await newTask.save();
+        res.status(200).json(newTask);
+    } catch {
+        console.status(500).json({ message: 'Server Error' });
+    }
+
+};
+
+//Get all tasks 
+exports.getAllTasks = async (req, res) => {
+    const userId = req.user.id;
+
+    try {
+        const task = await Task.find({ userId }).sort({ date: -1 });
+        res.status(200).json(task);
+    } catch (error) {
+        res.status(500).json({ message: 'Server Error' });
+    }
+};
+
+//Delete task
+exports.deleteTask = async (req, res) => {
+    try {
+        await Task.findByIdAndDelete(req.params.id);
+        res.json({ message: "Task deleted successfully" });
+    } catch (error) {
+        res.status(500).json({ message: 'Server Error' });
+    }
+};
+
+//Delete all tasks
+exports.deleteAllTasks = async (req, res) => {};
+
 exports.updateTask = async (req , res) =>{}
