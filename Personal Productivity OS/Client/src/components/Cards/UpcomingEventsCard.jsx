8e0fac0f41ca51660342eb1171b3b6861f6bcafe
ashutosh <kurvheashutosh@gmail.com>
2025-07-28 22:20:02 +0530

    Making some changes

--- Personal Productivity OS/Client/src/components/Cards/UpcomingEventsCard.jsx
+++ Personal Productivity OS/Client/src/components/Cards/UpcomingEventsCard.jsx
@@ -5,7 +5,7 @@ const UpcomingEventsCard = ({ title, startTime }) => {
     <div className="flex flex-col justify-between bg-white border-1 border-gray-200 pt-4 px-4 md:pt-4 md:pl-4 md:pr-0 rounded-2xl shadow-md shadow-gray-200 mb-4
      h-[100px] md:h-[100px] overflow-x-hidden">
       <h1 className="texe-xl">{title}</h1>
-      <div className="flex self-end md:flex-row px-2 md:p-2 bg-black rounded-t-4xl md:rounded-l-full">
+      <div className="flex self-end md:flex-row px-2  bg-black rounded-t-4xl md:rounded-l-full">
         <p className="text-sm px-2 py-1 rounded-full text-white">
           Start -
           <span className="ml-1">
